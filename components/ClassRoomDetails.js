cf1c7258b5bb33cebe0fce91126ead6999a93c55
makarroche <macarena@mynft.com>
2023-12-21 16:01:21 -0300

    Creation of room displaying

---
+++ components/ClassRoomDetails.js
@@ -0,0 +1,11 @@
+import { Row } from "react-bootstrap";
+
+const ClassRoomDetails = () => {
+  return (
+    <Row className="text-center">
+      <h3 className="text-white">Classroom Details</h3>
+    </Row>
+  );
+};
+
+export default ClassRoomDetails;
