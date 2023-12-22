b4a400966fc688e6532483ddb5e89ea453394cf1
makarroche <macarena@mynft.com>
2023-12-22 13:40:32 -0300

    Update ClassRoomDetails.js

--- components/ClassRoomDetails.js
+++ components/ClassRoomDetails.js
@@ -45,7 +45,7 @@ const ClassRoomDetails = ({ classroomDetails, students }) => {
                 participants.map((item) => {
                   return (
                     <p key={item.number}>
-                      Name: {item.name} - Student's Number: {item.number}
+                      Name: {item.name} - Students Number: {item.number}
                     </p>
                   );
                 })}
@@ -53,7 +53,7 @@ const ClassRoomDetails = ({ classroomDetails, students }) => {
             </ListGroup.Item>
 
             <ListGroup.Item className="bg-dark text-white">
-              Classroom's Capacity: {classroomDetails.capacity}
+              Classrooms Capacity: {classroomDetails.capacity}
             </ListGroup.Item>
           </ListGroup>
         </Col>
